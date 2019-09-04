c1694d26b5ad9cc5c1be350c742c2b3abadc238a
Kenya <kenya55@msn.com>
2019-09-04 13:04:32 -0400

    added html routes, added requires for dependencies and created draft survey

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//require(path.join(__dirname,"./app/routing/apiRoutes"))(app);
+require(path.join(__dirname,"./app/routing/htmlRoutes"))(app);
+
+app.listen(port,function(){
+    console.log("listening on port " + port) ; 
+});
